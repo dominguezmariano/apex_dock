6c0cb7e70808cb01850573bc1b3e5ec6b4232f5c
dominguezmariano <mariano.dominguez@despegar.com>
2026-05-27 10:12:41 -0300

    cambios en funcion de ambientes

--- apex-app/03_schema/06_functions/01_xx_apx_get_current_environment.sql
+++ apex-app/03_schema/06_functions/01_xx_apx_get_current_environment.sql
@@ -1,21 +1,24 @@
-create or replace FUNCTION xx_apx_get_current_environment 
+create or replace FUNCTION xx_apx_get_current_environment
 RETURN VARCHAR2
 IS
     v_environment VARCHAR2(200);
 BEGIN
     -- Determinar ambiente basado en diferentes criterios
-    IF UPPER(SYS_CONTEXT('USERENV', 'DB_NAME')) = 'EBSUAT' THEN 
+    IF UPPER(SYS_CONTEXT('USERENV', 'DB_NAME')) = 'FREEPDB1' THEN
+        v_environment := '';
+    ELSIF UPPER(SYS_CONTEXT('USERENV', 'DB_NAME')) = 'EBSUAT' THEN
         v_environment := '';
     ELSIF UPPER(SYS_CONTEXT('USERENV', 'DB_NAME'))  = 'EBST' THEN
         v_environment := 'Ejecutando aplicacion de TEST';
     ELSIF UPPER(SYS_CONTEXT('USERENV', 'DB_NAME')) = 'EBSD' THEN
         v_environment := 'Ejecutando aplicacion de DESARROLLO';
     ELSIF UPPER(SYS_CONTEXT('USERENV', 'DB_NAME')) = 'EBSTPF' THEN
-        v_environment := 'Ejecutando aplicacion de TESTFIX';    
+        v_environment := 'Ejecutando aplicacion de TESTFIX';
     ELSE
-        v_environment := 'DESCONOCIDO';
+        --v_environment := 'DESCONOCIDO';
+        v_environment := 'Ejecutando aplicacion en '||UPPER(SYS_CONTEXT('USERENV', 'DB_NAME'));
     END IF;
-    
+
     RETURN v_environment;
 END xx_apx_get_current_environment;
 /
